11d12236181651a124f4fce483f119a344c90fe9
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-10-09 20:50:12 +0530

    Modified : to create employee payroll table with columns

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -4,6 +4,15 @@ create database payroll_service;
 -- to use the database
 use payroll_service;
 
+-- create employee payroll table with columns id, name, salary and start date as column. Note Id is set to auto increment.
+create table employee_roll
+(
+id           int unsigned not null auto_increment,
+name         varchar(150) not null,
+salary       double not null,
+dateTime     datetime not null,
+primary key  (id)
+);
 
 
 
